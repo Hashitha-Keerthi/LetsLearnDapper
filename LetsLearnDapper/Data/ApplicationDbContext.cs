ede0e97e209ef6098c0f223edcc53db582280f97
Hashitha Keerthi <hkeerthi@icptechno.com>
2023-08-16 16:26:27 +0530

    Add project files.

---
+++ LetsLearnDapper/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using LetsLearnDapper.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LetsLearnDapper.Data
+{
+    public class ApplicationDbContext : DbContext
+    { 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Company> Companies { get; set; }  
+
+       
+    }
+}
